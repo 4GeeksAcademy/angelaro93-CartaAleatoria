0caccfb872ddfffbb9490437c7a3e57c48a5e8ff
angelaro93 <angela.ro.93@gmail.com>
2024-09-08 23:38:06 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,15 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let symbol = ["♦", "♥", "♠", "♣"];
+  let symbolTop = document.querySelector(".symbol-top");
+  let number = document.querySelector(".number");
+  let symbolBottom = document.querySelector(".symbol-bottom");
+  let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  let randomSymbol = symbol[Math.floor(Math.random() * symbol.length)];
+
+  number.textContent = randomNumber;
+  symbolTop.textContent = randomSymbol;
+  symbolBottom.textContent = randomSymbol;
 };
